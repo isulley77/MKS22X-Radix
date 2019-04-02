13572f86d421c6e119ce8dfd674c2efa7384f159
isulley77 <43052223+isulley77@users.noreply.github.com>
2019-04-02 14:34:03 -0400

    began radix

--- Radix.java
+++ Radix.java
@@ -1,7 +1,16 @@
 public class Radix{
-
-
-  public static void radixsort(int[]data){}
+  
+  public static void radixsort(int[]data){
+    
+    MyLinkedMist<Integer>[] = new MyLinkedList[20];
+    
+    for(Integer n : data){
+      if(n > 0){
+      }
+    
+    }
+  
+  }
   
   
   public static void main(String[] args){}
