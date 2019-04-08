af9916e8d7eab78b28f8b435ef4827202543ddfd
isulley77 <43052223+isulley77@users.noreply.github.com>
2019-04-07 20:37:54 -0400

    updated radixSort()

--- Radix.java
+++ Radix.java
@@ -1,14 +1,28 @@
 public class Radix{
   
-  public static void radixsort(int[]data){
+  public static void radixsort(int[] data){
     
-    MyLinkedList<Integer>[] = new MyLinkedList[20];
-    
-    for(Integer n : data){
-      if(n > 0){
+    if(data.size != 0){
+      @SuppressWarnings("unchecked") MyLinkedList<Integer>[] dig = new MyLinkedList[20];
+      
+      for(int i = 0; i < 20; i++){
+        dig[i] = new MyLinkedList<Integer>();
       }
+   
+      MyLinkedList<Integer> pos = new MyLinkedList<>();
     
+    for(int n : data){
+      if(n > 0){
+        pos.add(n);
+        pos.remove(n);
+      }
     }
+      
+      if(pos.size() > 0){
+      
+      for(int i = 1; 
+        
+      }
   
   }
   
